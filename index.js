c7de390df6dc032daaabd1da12b0956803221ab0
DelJuan Smith <deljuans66@gmail.com>
2025-10-03 03:17:18 -0400

    Vault secured

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 let code1;
-code1=7+3;
+code1 = 7 + 3;
 
-let code2 = "100/2-10";
+let code2 = 100 / 2 - 10;
 
-let code3 = "200%161";
+let code3 = 200 % 161;
 
-let message = "The vault has been secured. The combination is:"
+let message = "The vault has been secured. The combination is:";
 
-let code A = "code1" + "code2" + "code3";
+let codeA = code1 + "-" + code2 + "-" + code3;
 
-code B = 
+let codeB = `${code1}-${code2}-${code3}`;
 
-
+console.log(message);
+console.log(codeA);
+console.log(codeB);
